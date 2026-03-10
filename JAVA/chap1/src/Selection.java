505cb2ccd69c5376dc8d94a2db80316d66a9cd57
NotHyoone <gydnjs3505@gmail.com>
2026-03-11 00:28:50 +0900

    clean

--- JAVA/chap1/src/Selection.java
+++ JAVA/chap1/src/Selection.java
@@ -1,8 +1,6 @@
 public class Selection extends AbstractSort {
     public static void sort(Comparable[] a) {
         int N = a.length;
-        System.out.println("Before sorting:");
-        show(a);
         for (int i = 0; i < N-1; i++) {
             int min = i;
             for (int j = i+1; j < N; j++) {
@@ -11,15 +9,13 @@ public class Selection extends AbstractSort {
             }
             exch(a, i, min);
         }
-        System.out.println("After sorting:");
-        show(a);
-        System.out.println("Is sorted? " + isSorted(a));
         assert isSorted(a);
-    };
+    }
 
     public static void main(String[] args) {
         Integer[] a = {10, 4, 5, 2, 1, 8, 3, 6};
         Selection.sort(a);
         Selection.show(a);
+
     }
 }
