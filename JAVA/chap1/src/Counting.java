74efc622904570d9d2d230591e2f2fd2d7260fb0
Hyowon An <gydnjs3505@gmail.com>
2026-03-11 14:05:41 +0900

    feat : counting sorted 주석 추가

--- JAVA/chap1/src/Counting.java
+++ JAVA/chap1/src/Counting.java
@@ -3,17 +3,19 @@ public class Counting {
         int i, N = A.length;
         int[] C = new int[K], B = new int[N];
 
-        for (i=0; i<N; i++) C[A[i]]++;
-        for (i=1; i<K; i++) C[i] += C[i-1];
-        for (i=N-1; i >=0; i--)
-            B[--C[A[i]]] = A[i];
+        // 각 값의 등장 횟수를 셈
+        for (i = 0; i < N; i++) C[A[i]]++;
+        // 누적합으로 변환하여 각 원소의 출력 위치를 계산``
+        for (i = 1; i < K; i++) C[i] += C[i-1];
+        // 역순으로 A를 순회하며 B에 정렬된 결과를 저장 (안정 정렬)
+        for (i = N-1; i >= 0; i--) B[--C[A[i]]] = A[i];
         return B;
     }
 
     public static void main(String[] args) {
         int[] A = {10, 4, 5, 8, 1, 8, 3, 6}, B;
         B = Counting.sort(A, 11);
-        for (int i=0; i<B.length; i++)
+        for (int i = 0; i < B.length; i++)
             System.out.print(B[i] + " ");
         System.out.println();
     }
--- JAVA/chap1/src/Insertion.java
+++ JAVA/chap1/src/Insertion.java
@@ -1,13 +1,13 @@
 public class Insertion extends AbstractSort {
     public static void sort(Comparable[] a) {
         int N = a.length;
-
-        for (int i = 0; i < N; i++) {
+        for (int i = 1; i < N; i++) {
             for (int j = i; j > 0 && less(a[j], a[j-1]); j--) {
                 exch(a, j, j-1);
             }
         }
         assert isSorted(a);
+
     }
 
     public static void main(String[] args) {
