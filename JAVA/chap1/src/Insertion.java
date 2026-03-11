934faebef056a16d3b2690ccbc6cac61b48a523e
Hyowon An <gydnjs3505@gmail.com>
2026-03-11 12:44:40 +0900

    clean update

--- JAVA/chap1/src/Insertion.java
+++ JAVA/chap1/src/Insertion.java
@@ -1,9 +1,11 @@
 public class Insertion extends AbstractSort {
     public static void sort(Comparable[] a) {
         int N = a.length;
-        for (int i = 1; i < N; i++) {
-            for (int j = i; j > 0 && less(a[j], a[j-1]); j--)
+
+        for (int i = 0; i < N; i++) {
+            for (int j = i; j > 0 && less(a[j], a[j-1]); j--) {
                 exch(a, j, j-1);
+            }
         }
         assert isSorted(a);
     }
--- JAVA/chap1/src/Shell.java
+++ JAVA/chap1/src/Shell.java
@@ -2,9 +2,9 @@ public class Shell extends AbstractSort {
     public static void sort(Comparable[] a) {
         int N = a.length;
         int h = 1;
-        while (h < N/3) h = h*3 + 1;
+        while(h < N/3) h = h*3 + 1;
 
-        while (h >= 1) {    // 1-sorted array 까지 와야하므로 h >= 1 이 조건이 됨
+        while(h >= 1) {
             for (int i = h; i < N; i++) {
                 for (int j = i; j >= h && less(a[j], a[j-h]); j -= h) {
                     exch(a, j, j-h);
