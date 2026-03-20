36b0c9b3c7e1268847854ae3c3e34bd154ea2fe7
NotHyoone <gydnjs3505@gmail.com>
2026-03-21 00:36:10 +0900

    SequentialSearch Symbol Table 구현 연습 중

--- JAVA/chap2/src/SequentialSearchST2.java
+++ JAVA/chap2/src/SequentialSearchST2.java
@@ -1,42 +1,49 @@
 import java.util.ArrayList;
 
-class Node <K, V> {
+class Node2<K, V> {
     K key;
     V value;
     Node<K, V> next;
-
-    public Node(K key, V value, Node<K, V> next) {
+    public Node2(K key, V value, Node<K, V> next) {
         this.key = key;
         this.value = value;
         this.next = next;
     }
 }
 
-public class SequentialSearchST<K, V> {
+public class SequentialSearchST2<K, V> {
     private Node<K, V> first;
-    private int N = 0; // 연결 리스트 노드 수
+    private int N = 0;
 
     public V get(K key) {
-        for (Node<K, V> x = first; x != null; x = x.next)
-            if (key.equals(x.key)) return x.value;
+        for (Node<K, V> x = first; x != null; x = x.next) {
+            if (key.equals(x.key)) {
+                return x.value;
+            }
+        }
         return null;
     }
     public void put(K key, V value) {
-        for (Node<K, V> x=first; x != null; x=x.next)
+        // 키가 있는 경우
+        for (Node<K, V> x = first; x != null; x=x.next) {
             if (key.equals(x.key)) {
-                x.value = value; return;
+                x.value = value;
+                return;
             }
-        first = new Node<K, V>(key, value, first);
+        }
+        first = new Node<K, V>(key, value, first);  // 키 없으면, 앞에 노드 추가
         N++;
     }
     public void delete(K key) {
-        // 첫 번째 노드 삭제하는 경우
-        if (key.equals(first.key)) {
-            first = first.next; N--; return;
+        // 맨 앞 삭제
+        if (key.equals(first.key)){
+            first = first.next;
+            N--;
+            return;
         }
 
-        for (Node<K, V> x = first; x.next != null; x = x.next) {
-            if (key.equals(x.next.key)) {
+        for (Node<K, V> x=first; x.next != null; x=x.next) {
+            if(key.equals(x.next.key)) {
                 x.next = x.next.next;
                 N--;
                 return;
@@ -45,8 +52,9 @@ public class SequentialSearchST<K, V> {
     }
     public Iterable<K> keys() {
         ArrayList<K> keyList = new ArrayList<K>(N);
-        for (Node<K, V> x=first; x != null; x=x.next)
+        for (Node<K, V> x=first; x!=null; x=x.next) {
             keyList.add(x.key);
+        }
         return keyList;
     }
     public boolean contains(K key) {
